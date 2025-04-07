b9dd012fdd67d19a7d2027e1708b93dca0593efd
FireFlyCL <ni.castillor@duocuc.cl>
2025-04-07 02:55:32 -0400

    css solicitudes

--- src/app/components/dashboard-solicitud/dashboard-solicitud.component.ts
+++ src/app/components/dashboard-solicitud/dashboard-solicitud.component.ts
@@ -1,27 +1,44 @@
 import { Component } from '@angular/core';
 import { MatSelectionListChange } from '@angular/material/list';
 import { Router } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
-    selector: 'app-dashboard-solicitud',
-    templateUrl: './dashboard-solicitud.component.html',
-    styleUrls: ['./dashboard-solicitud.component.css'],
-    standalone: false
+  selector: 'app-dashboard-solicitud',
+  templateUrl: './dashboard-solicitud.component.html',
+  styleUrls: ['./dashboard-solicitud.component.css'],
+  standalone: false,
 })
 export class DashboardSolicitudComponent {
-  typesOfShoes: { name: string, route: string }[] = [
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  isSmallScreen: boolean = false;
+
+  typesOfShoes: { name: string; route: string }[] = [
     { name: 'Solicitudes', route: '/dashboard/productos' },
-    { name: 'Volver Atrás', route: '/perfil' }
+    { name: 'Volver Atrás', route: '/perfil' },
   ];
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isSmallScreen = result.matches;
+      });
+  }
 
   onSelection(event: MatSelectionListChange): void {
     if (event.options[0].selected) {
       const selectedShoe = event.options[0].value;
       this.router.navigate([selectedShoe.route]);
+
+      if (this.isSmallScreen) {
+        this.sidenav.close(); // Cierra el sidenav si es pantalla pequeña
+      }
     }
   }
-
-  
 }
